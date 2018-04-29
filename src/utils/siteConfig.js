0b6bcd964472c0c45d70fa232d12510d2fd5b17a
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2018-04-29 17:37:39 +0200

    Development changes

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
   siteTitle: 'Kevin Van Ransbeeck',
-  siteDescription: 'Personal blog built on top of Gatsby and Contentful.',
+  siteDescription: 'Personal blog built on top of Gatsby and Contentful',
   siteUrl: 'https://kevin.van-ransbeeck.be' // Site domain. Do not inculde a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
 };
